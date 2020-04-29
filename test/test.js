4e11b70d3b812a1355676021cce0d082562662e1
Girish Ramakrishnan <girish@cloudron.io>
2020-04-29 16:24:04 -0700

    disable strict host key check

--- test/test.js
+++ test/test.js
@@ -164,7 +164,7 @@ describe('Application life cycle test', function () {
             //     StrictHostKeyChecking no
             //     HashKnownHosts no
             execSync(util.format('sed -i \'/%s/d\' -i ~/.ssh/known_hosts', app.fqdn));
-            const sftpCommand = `sshpass -p${process.env.PASSWORD} sftp -P 222 -oBatchMode=no -b - ${process.env.USERNAME}@${app.fqdn}@${apiEndpoint}`;
+            const sftpCommand = `sshpass -p${process.env.PASSWORD} sftp -P 222 -o StrictHostKeyChecking=no -oBatchMode=no -b - ${process.env.USERNAME}@${app.fqdn}@${apiEndpoint}`;
             console.log('If this test fails, see the comment above this log message. Run -- ', sftpCommand);
             execSync(sftpCommand, { input: 'cd public\nput test.php\nbye\n', encoding: 'utf8', cwd: __dirname });
         });
@@ -214,7 +214,7 @@ describe('Application life cycle test', function () {
         it('can upload file with sftp', function () {
             // remove from known hosts in case this test was run on other apps with the same domain already
             // if the tests fail here you want to set "HashKnownHosts no" in ~/.ssh/config
-            const sftpCommand = `sshpass -p${process.env.PASSWORD} sftp -P 222 -oBatchMode=no -b - ${process.env.USERNAME}@${app.fqdn}@${apiEndpoint}`;
+            const sftpCommand = `sshpass -p${process.env.PASSWORD} sftp -P 222 -o StrictHostKeyChecking=no -oBatchMode=no -b - ${process.env.USERNAME}@${app.fqdn}@${apiEndpoint}`;
             console.log('If this test fails, see the comment above this log message. Run -- ', sftpCommand);
             execSync(sftpCommand, { input: 'cd public\nput test.php\nbye\n', encoding: 'utf8', cwd: __dirname });
         });
